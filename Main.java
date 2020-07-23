c0ad720135d27366c84dd9c79894d8f448dbf20b
Britta <50714303+DorkLordCthulhu@users.noreply.github.com>
2020-07-23 09:40:18 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package datapull;
+
+public class Main {
+
+    public static void main(String[] args) {
+	datapull.PullInfo inquiry = new datapull.PullInfo();
+        inquiry.setRequestedInfo();
+        inquiry.getDocInfo();
+    }
+}
